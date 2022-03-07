cda7fd27bb48e35f3812ea52724aa8c6dc4d7d94
Shubham <shubham20091999@gmail.com>
2022-03-07 23:41:05 +0530

    some setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+extern crate console_error_panic_hook;
+
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -12,6 +15,145 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+}
+
+use web_sys::{
+    WebGl2RenderingContext, WebGlFramebuffer, WebGlProgram, WebGlShader, WebGlTexture, Window,
+};
+
+mod utils;
+
+use WebGl2RenderingContext as GL;
+//-------------------------------------------------------------
+
+const PXSIZE: u32 = 4;
+
+const VERT_SHADER: &str = r##"#version 300 es
+ 
+in vec4 position;
+
+void main() {
+
+    gl_Position = position;
+}
+"##;
+
+const COMPUTE_FRAG_SHADER: &str = r##"#version 300 es
+
+precision lowp float;
+precision lowp int;
+
+uniform sampler2D u_texture;
+uniform vec2 u_size;
+
+out vec4 outColor;
+
+vec4 getValueAt(int i, int j) {
+    return texelFetch(u_texture, ivec2(mod((gl_FragCoord.xy) - vec2(i, j), u_size)), 0);
+}
+
+int getAliveOrDeadAt(int i, int j) {
+    return int(getValueAt(i, j).r > 0.50);
+}
+
+void main() {
+    int isAlive = getAliveOrDeadAt(0, 0);
+    float isAlive_f = float(isAlive);
+    int aliveNeighbourCount = getAliveOrDeadAt(1, 1) + getAliveOrDeadAt(1, -1) + getAliveOrDeadAt(-1, 1) + getAliveOrDeadAt(-1, -1) + getAliveOrDeadAt(0, 1) + getAliveOrDeadAt(1, 0) + getAliveOrDeadAt(0, -1) + getAliveOrDeadAt(-1, 0);
+
+    float willBeAlive = float((aliveNeighbourCount == 3) || (bool(isAlive) && aliveNeighbourCount == 2));
+
+    //For diming effect for newly alive cell and newly dead cells
+    // outColor.r = willBeAlive = willBeAlive * (isAlive_f + 0.60) + (isAlive_f - 0.85) * (1.0 - willBeAlive);
+    // isAlive_f - (1.0-a) + (1.0-a+b) * willBeAlive;
+    // a-> brightness of willBeDead pixel
+    // b-> brightness of will Be alive pixel
+    outColor.r = isAlive_f - 0.85 + 1.45 * willBeAlive;
+}
+"##;
+
+const DISPLAY_FRAG_SHADER: &str = r##"#version 300 es
+
+precision highp float;
+
+uniform sampler2D u_texture;
+uniform float u_px_size;
+
+out vec4 outColor;
+
+void main() {
+    float val = texelFetch(u_texture, ivec2(gl_FragCoord.xy / u_px_size), 0).r;
+    outColor = vec4(val,val,0.0,1.0);
+    // outColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"##;
+
+//-------------------------------------------------------------
+struct Program<T> {
+    display: T,
+    compute: T,
+}
+
+struct Size {
+    width: u32,
+    height: u32,
+}
+#[wasm_bindgen]
+pub struct GOL {
+    gl: GL,
+    size: Program<Size>,
+    program: Program<WebGlProgram>,
+}
+
+impl GOL {
+    pub fn new() -> Self {
+        let window = utils::get_window();
+
+        let gl = utils::get_canvas("canvas")
+            .get_context("webgl2")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<GL>()
+            .unwrap();
+
+        let display_size = Size {
+            height: window.inner_height().unwrap().as_f64().unwrap() as u32,
+            width: window.inner_width().unwrap().as_f64().unwrap() as u32,
+        };
+
+        let compute_size = Size {
+            height: display_size.height / PXSIZE,
+            width: display_size.width / PXSIZE,
+        };
+
+        let vert_shader = utils::compile_shader(&gl, GL::VERTEX_SHADER, VERT_SHADER)
+            .ok()
+            .unwrap();
+        let compute_frag = utils::compile_shader(&gl, GL::FRAGMENT_SHADER, COMPUTE_FRAG_SHADER)
+            .ok()
+            .unwrap();
+        let disp_frag = utils::compile_shader(&gl, GL::FRAGMENT_SHADER, DISPLAY_FRAG_SHADER)
+            .ok()
+            .unwrap();
+
+        let program = Program {
+            display: utils::link_program(&gl, &vert_shader, &disp_frag)
+                .ok()
+                .unwrap(),
+            compute: utils::link_program(&gl, &vert_shader, &compute_frag)
+                .ok()
+                .unwrap(),
+        };
+
+        return Self {
+            gl: gl,
+            size: Program {
+                display: display_size,
+                compute: compute_size,
+            },
+            program: program,
+        };
+    }
 }
 
 // This is like the `main` function, except for JavaScript.
---
+++ src/utils.rs
@@ -0,0 +1,122 @@
+use wasm_bindgen::JsCast;
+
+use web_sys::{
+    Document, HtmlCanvasElement, WebGl2RenderingContext, WebGlFramebuffer, WebGlProgram,
+    WebGlShader, WebGlTexture, Window,
+};
+
+pub fn get_window() -> Window {
+    return web_sys::window().unwrap();
+}
+
+pub fn get_document() -> Document {
+    return get_window().document().unwrap();
+}
+
+pub fn get_canvas(id: &str) -> HtmlCanvasElement {
+    let canvas = get_document().get_element_by_id(id).unwrap();
+    return canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+}
+
+pub fn compile_shader(
+    gl: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = gl
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))
+        .unwrap();
+    gl.shader_source(&shader, source);
+    gl.compile_shader(&shader);
+
+    if gl
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(gl
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    gl: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = gl
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))
+        .unwrap();
+
+    gl.attach_shader(&program, vert_shader);
+    gl.attach_shader(&program, frag_shader);
+    gl.link_program(&program);
+
+    if gl
+        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(gl
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
+
+pub fn create_texture(
+    gl: &WebGl2RenderingContext,
+    sizew: i32,
+    sizeh: i32,
+    data: Option<&js_sys::Object>,
+) -> Result<WebGlTexture, String> {
+    let texture = gl.create_texture();
+    if let None = texture {
+        return Err("unable to initialize".to_string());
+    }
+    gl.active_texture(WebGl2RenderingContext::TEXTURE0 + 0);
+    gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, texture.as_ref());
+
+    gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view(
+        WebGl2RenderingContext::TEXTURE_2D,
+        0,
+        WebGl2RenderingContext::R8 as i32,
+        sizew,
+        sizeh,
+        0,
+        WebGl2RenderingContext::RED,
+        WebGl2RenderingContext::UNSIGNED_BYTE,
+        data,
+    )
+    .map_err(|err| println!("{:?}", err))
+    .ok();
+
+    gl.tex_parameteri(
+        WebGl2RenderingContext::TEXTURE_2D,
+        WebGl2RenderingContext::TEXTURE_MIN_FILTER,
+        WebGl2RenderingContext::NEAREST as i32,
+    );
+    gl.tex_parameteri(
+        WebGl2RenderingContext::TEXTURE_2D,
+        WebGl2RenderingContext::TEXTURE_MAG_FILTER,
+        WebGl2RenderingContext::NEAREST as i32,
+    );
+    gl.tex_parameteri(
+        WebGl2RenderingContext::TEXTURE_2D,
+        WebGl2RenderingContext::TEXTURE_WRAP_S,
+        WebGl2RenderingContext::REPEAT as i32,
+    );
+    gl.tex_parameteri(
+        WebGl2RenderingContext::TEXTURE_2D,
+        WebGl2RenderingContext::TEXTURE_WRAP_T,
+        WebGl2RenderingContext::REPEAT as i32,
+    );
+
+    return Ok(texture.unwrap());
+}
