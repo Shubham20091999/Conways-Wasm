82dd76fe9650b8ff9e0d07c9a4e791754945c887
Shubham <shubham20091999@gmail.com>
2022-03-07 23:14:48 +0530

    Hello world

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
-
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,6 +8,11 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -19,9 +22,8 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    log("Hello world!");
 
     Ok(())
 }
