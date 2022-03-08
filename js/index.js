72645fcbdce8a0739499c1ad21b8f1b4ac83c408
Shubham <shubham20091999@gmail.com>
2022-03-08 10:28:59 +0530

    frame rate limiter added

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ let rust = import("../pkg/index.js");
 
 document.getElementById("main")
 
+//milli seconds per frame
+const MSpF = 100;
+
 rust.then(m => {
     const gol = new m.GOL();
 
-    let drawScene = function(){
-        gol.draw();
+    let last_frame = Number.NEGATIVE_INFINITY;
+    let drawScene = function (time) {
         requestAnimationFrame(drawScene);
-    } 
-    // setInterval(m => { gol.draw() }, 100);
-    requestAnimationFrame(drawScene);
+        if ((time - last_frame) > MSpF) {
+            gol.draw();
+            last_frame = time;
+        }
+    }
+    drawScene();
 }).catch(console.error);
 
